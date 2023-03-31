953979f02985f14b433a685cf26a879784b048c0
madsnbkristensen <madskristensen@hotmail.dk>
2023-03-31 13:50:47 +0200

    update

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,12 +1,11 @@
-
 //Hamburger Menu
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-} )
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
 
 
 
@@ -19,3 +18,17 @@ bars.forEach((bar) => {
   const height = bar.getAttribute('data-height');
   bar.style.height = `${height}%`;
 });
+
+
+
+//graph animation
+const graphBar1 = document.querySelector(".bar1")
+
+const observer = new IntersectionObserver(entries => {})
+
+observer.observe(document.querySelector(".bar1"))
+
+entries.forEach(entry => {
+  if (entry.isIntersecting)
+    entry.target.classList.add("graph-bar-animation")
+})
