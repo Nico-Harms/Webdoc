7a21dafa1747845922aed3903a55535ca7f881a8
Nicolai Harms <Nicolai.harms@gmail.com>
2023-03-31 13:04:43 +0200

    Graph menu

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,3 +7,15 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 } )
+
+
+
+
+// Get all the bar elements
+const bars = document.querySelectorAll('.bar');
+
+// Loop through the bars and set the height of each bar based on its data-height attribute
+bars.forEach((bar) => {
+  const height = bar.getAttribute('data-height');
+  bar.style.height = `${height}%`;
+});
