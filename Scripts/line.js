9bf5476de540775047bf434c3f2665212d84469e
Nicolai Harms <Nicolai.harms@gmail.com>
2023-04-13 12:53:09 +0200

    line update

--- Scripts/line.js
+++ Scripts/line.js
@@ -9,13 +9,32 @@ triangle.style.strokeDasharray = length;
 triangle.style.strokeDashoffset = length;
 
 // Find scroll percentage on scroll (using cross-browser properties), and offset dash same amount as percentage scrolled
+
+var scrolledIntoView = false;
+var scale = 3;
+
 window.addEventListener("scroll", myFunction);
 
 function myFunction() {
-    var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+  var elementTop = triangle.getBoundingClientRect().top;
 
-    var draw = length * scrollpercent;
+  if (!scrolledIntoView && elementTop < window.innerHeight) {
+    // The triangle is within the top of the viewport, so set scrolledIntoView to true
+    scrolledIntoView = true;
+  }
+
+  if (scrolledIntoView) {
+    // Calculate the scroll percentage with a 0.5 offset to delay the start of the scroll effect
+    var scrollpercent = ((document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) - 0.5;
+
+    // Limit the scrollpercent value between 0 and 1
+    scrollpercent = Math.max(0, Math.min(1, scrollpercent));
+
+    // Calculate the amount to draw based on the scroll percentage and the scale factor
+    var draw = length * scrollpercent * scale;
 
     // Reverse the drawing (when scrolling upwards)
     triangle.style.strokeDashoffset = length - draw;
+  }
 }
+
