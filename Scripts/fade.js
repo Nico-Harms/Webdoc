c8f39fa1779184da6ed9c4f25be80e220932d551
madsnbkristensen <madskristensen@hotmail.dk>
2023-04-17 12:08:19 +0200

    dd

--- Scripts/fade.js
+++ Scripts/fade.js
@@ -1,17 +1,17 @@
 // Fade Animations
-const faders = document.querySelectorAll('.fade-in'); 
-const sliders = document.querySelectorAll('slide-in'); 
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('slide-in');
 const appearOptions = {
   threshold: 0,
   rootMargin: "0px 0px -300px 0px"
 };
-const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
   entries.forEach(entry => {
     if (!entry.isIntersecting) {
       return;
     } else {
-       entry.target.classList.add('appear');
-       appearOnScroll.unobserve(entry.target);
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
     }
   })
 }, appearOptions);
---
+++ Scripts/fade2.js
@@ -0,0 +1,25 @@
+// Fade Animations
+const faders2 = document.querySelectorAll('.fade-in-2');
+const sliders2 = document.querySelectorAll('slide-in');
+const appearOptions2 = {
+    threshold: 0,
+    rootMargin: "0px 0px -100px 0px"
+};
+const appearOnScroll2 = new IntersectionObserver(function (entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('inuse');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions2);
+
+faders2.forEach(fader => {
+    appearOnScroll2.observe(fader);
+});
+
+sliders2.forEach(slider => {
+    appearOnScroll2.observe(slider);
+});
