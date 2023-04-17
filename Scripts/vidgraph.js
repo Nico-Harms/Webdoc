5f6d9d104e3e46d9d531e8e085a6976a883b9566
TobiasRW <tobiasrw98@gmail.com>
2023-04-17 10:38:25 +0200

    graph vid play in view

---
+++ Scripts/vidgraph.js
@@ -0,0 +1,11 @@
+const graphVid = document.getElementById('graph-video');
+var element = document.getElementById('graph-vid-sect');
+
+window.addEventListener('scroll', function() {
+    if (element.getBoundingClientRect().top < window.innerHeight && element.getBoundingClientRect().bottom > 0 ) {
+      graphVid.play();
+    }
+    else {
+      graphVid.pause();
+    }
+})
