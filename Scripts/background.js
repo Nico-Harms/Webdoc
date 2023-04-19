eed226794803f14ce5f3bd4a562bfb3a09be0acd
Nicolai Harms <Nicolai.harms@gmail.com>
2023-04-19 12:58:13 +0200

    --

--- Scripts/background.js
+++ Scripts/background.js
@@ -3,13 +3,7 @@
 
 const handleScroll = (event) => {
     const scrollPosition = event.target.scrollingElement.scrollTop;
-  
-    console.log("handleScroll");
-  
     const images = document.querySelectorAll(".parallax-img");
-  
-    console.log("images", images);
-  
     images.forEach((element) => {
       element.style.transform = `translate(0, ${scrollPosition / 25}px)`;
     });
